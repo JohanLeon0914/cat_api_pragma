7f2360ae2e11aca2603237a3d21a40351f9be9a6
JohanLeon0914 <64605783+JohanLeon0914@users.noreply.github.com>
2023-09-29 08:53:54 -0500

    agregando variables de entorno

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Cat } from '../models/Cat';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'https://api.thecatapi.com/v1/breeds';
-  private apiKey = 'bda53789-d59e46cd-9bc4-2936630fde39';
+  private apiUrl = environment.apiUrl;
+  private apiKey = environment.apiKey;
 
   constructor(private http: HttpClient) {}
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.thecatapi.com/v1/breeds',
+  apiKey: 'bda53789-d59e46cd-9bc4-2936630fde39',
 };
